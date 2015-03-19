7a5f96098266ec7e4f5a84cba89411d9efff2331
Jacob Stuart <stuart4@purdue.edu>
2015-03-18 22:20:00 -0400

    just the beginning

--- md5.c
+++ md5.c
@@ -1,2 +1,29 @@
-int main() {
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char * expand(char * message) {
+	int messageSize = strlen(message);
+	int newSize = 448 - messageSize % 512 + messageSize + 4;
+	char * new = realloc(message, newSize);
+	if (messageSize < newSize - 5) {
+		new[messageSize++] = 0x80;
+	}
+	while (messageSize < newSize - 4) {
+		new[messageSize++] = 0x00;
+	}
+	//TODO end 64bit num representing original string
+	return new;
+}
+
+int main(int argc, char ** argv) {
+	if (argc != 2) {
+		printf("usage ./md5 \"message\"\n");
+		exit(1);
+	}
+
+	char * new = expand(strdup(argv[1]));
+
+	return 0;
+
 }
